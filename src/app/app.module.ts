140c8fffb0199ab429181edb96297b7dd4320208
delaeran <fofw@hotmail.fr>
2023-07-26 10:43:03 +0200

    Reprise projet, creation service Mock faction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeDisplayComponent } from './liste-display/liste-display.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddUnitComponent } from './add-unit/add-unit.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { CreationArmeeComponent } from './creation-armee/creation-armee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeDisplayComponent,
+    AddUnitComponent,
+    MainMenuComponent,
+    CreationArmeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
